a8664b66918dfd87540ae72ff95a214ec74f6fc7
Gilmar Oliveira <gilmar.oliveira@fcamara.com.br>
2017-11-12 20:01:44 -0200

    Condomínio Front (Post e Update)

--- src/SgConWeb/src/app/layout/condominium/condominium-edit/condominium-edit.component.ts
+++ src/SgConWeb/src/app/layout/condominium/condominium-edit/condominium-edit.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { ActivatedRoute } from '@angular/router';
+
 // Services
 import { CondominiumService } from '../../../shared/services/condominium.service';
+
 // Models
 import { CondominiumModel } from '../../../shared/entities/condominium.model';
 
@@ -21,18 +23,33 @@ export class CondominiumEditComponent implements OnInit {
 
   constructor(
     private form: FormBuilder,
-    private _condominiumService: CondominiumService
+    private _condominiumService: CondominiumService,
+    private _route: ActivatedRoute
   ) { }
 
   ngOnInit() {
+    this.setDefaultValuesForm();
+
+    this._route.params.subscribe(params => {
+
+      if (params['id']) {
+        this.condominiumId = +params['id'];
+        this.getCondominium(this.condominiumId);
+      }
+    });
+
+  }
+
+  setDefaultValuesForm() {
+
     this.condominiumForm = this.form.group({
       name: [null],
       email: [null],
       cnpj: [null],
-      dddComercialPhone: [null],
+      DDDComercialPhone: [null],
       comercialPhone: [null],
-      dddCelPhone: [null],
-      celPhone: [null],
+      DDDCellPhone: [null],
+      cellPhone: [null],
       street: [null],
       number: [null],
       cep: [null],
@@ -47,10 +64,10 @@ export class CondominiumEditComponent implements OnInit {
 
     this._condominiumService.consultingCep(cep)
       .map(data => data.json())
-      .subscribe(data => this.populadataForm(data, this.condominiumForm));
+      .subscribe(data => this.populateAddressForm(data, this.condominiumForm));
   }
 
-  populadataForm(data, condominiumForm) {
+  populateAddressForm(data, condominiumForm) {
     this.condominiumForm.patchValue({
       cep: data.cep,
       city: data.localidade,
@@ -60,8 +77,56 @@ export class CondominiumEditComponent implements OnInit {
       uf: data.uf
     })
   }
+
+  populateForm(data: CondominiumModel) {
+
+    this.condominiumForm.patchValue({
+      name: data.name,
+      email: data.email,
+      cnpj: data.cnpj,
+      DDDComercialPhone: data.dddComercialPhone,
+      comercialPhone: data.comercialPhone,
+      DDDCellPhone: data.dddCellPhone,
+      cellPhone: data.cellPhone,
+      cep: data.cep,
+      street: data.street,
+      number: data.number,
+      neighborhood: data.neighborhood,
+      city: data.city,
+      complement: data.complement,
+      uf: data.uf
+    });
+
+  }
+
   onSubmit() {
-    console.log(this.condominiumForm.value);
+
+    this.condominiumModel = this.condominiumForm.value;
+
+    if (!this.condominiumId) {
+      this._condominiumService.postCondominium(this.condominiumModel)
+        .subscribe(response => {
+          this.condominiumModel = response;
+          console.log('salvo com sucesso');
+        }, error => {
+          console.log(error);
+        });
+    } else {
+      this._condominiumService.updateCondominium(this.condominiumModel, this.condominiumId)
+        .subscribe(response => {
+          this.condominiumModel = response;
+          console.log('atualizado com sucesso');
+        }, error => {
+          console.log(error);
+        });
+    }
+
+    this._condominiumService.postCondominium(this.condominiumModel)
+      .subscribe(response => {
+        this.condominiumModel = response;
+      }, error => {
+        console.log(error);
+      });
   }
 
   getCondominium(id) {
@@ -69,7 +134,7 @@ export class CondominiumEditComponent implements OnInit {
     this._condominiumService.getCondominiumById(id)
       .subscribe(response => {
         this.condominiumModel = response;
-        console.log(response);
+        this.populateForm(this.condominiumModel);
       }, error => {
         console.log(error);
       });
--- src/SgConWeb/src/app/shared/entities/condominium.model.ts
+++ src/SgConWeb/src/app/shared/entities/condominium.model.ts
@@ -3,16 +3,16 @@ export class CondominiumModel {
     name: string;
     cnpj: string;
     email: string;
-    DDDComercialPhone: string;
-    ComercialPhone: string;
-    DDDCellPhone: string;
-    CellPhone: string;
+    dddComercialPhone: string;
+    comercialPhone: string;
+    dddCellPhone: string;
+    cellPhone: string;
     towerNumber: number;
-    CEP: string;
+    cep: string;
     street: string;
     number: number;
     complement: string;
     neighborhood: string;
     city: string;
-    UF: string;
+    uf: string;
 }
