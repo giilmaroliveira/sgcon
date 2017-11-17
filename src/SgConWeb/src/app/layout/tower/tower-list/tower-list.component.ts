0d3ca07669651fc6746f46dda3bdad333f35ecef
Fabio Jr Silva <fabiopt6@gmail.com>
2017-11-17 18:14:19 -0200

    Buscar Torres

--- src/SgConWeb/src/app/layout/tower/tower-list/tower-list.component.ts
+++ src/SgConWeb/src/app/layout/tower/tower-list/tower-list.component.ts
@@ -5,6 +5,8 @@ import { TowerService } from '../../../shared/services/tower.service';
 
 // models
 import { TowerModel } from '../../../shared/entities/tower.model';
+import { CondominiumModel } from '../../../shared/entities/condominium.model';
+import { CondominiumService } from '../../../shared/services/condominium.service';
 
 @Component({
   selector: 'app-tower-list',
@@ -13,17 +15,33 @@ import { TowerModel } from '../../../shared/entities/tower.model';
 })
 export class TowerListComponent implements OnInit {
   towerList: TowerModel[] = new Array<TowerModel>();
+  listOfCondominium: CondominiumModel[] = new Array<CondominiumModel>();
+  condominiumId: number;
 
-  constructor(private _towerService: TowerService) { }
+  constructor(
+    private _towerService: TowerService,
+    private _condominiumService: CondominiumService
+  ) {}
 
   ngOnInit() {
-    this.getAllTower();
+    this.getAllCondominium();
   }
   getAllTower() {
     this._towerService.getAllTower().subscribe(
       response => {
         this.towerList = response;
         console.log(response);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  getAllCondominium() {
+    this._condominiumService.getAllCondominium().subscribe(
+      response => {
+        this.listOfCondominium = response;
       },
       error => {
         console.log(error);
