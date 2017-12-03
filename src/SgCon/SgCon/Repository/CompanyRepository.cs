5f6a69e65e21d3025cab310badb7144d945ac26c
Gilmar Oliveira <gilmar.oliveira@fcamara.com.br>
2017-12-03 00:19:45 -0200

    Company and Employee Repository

--- src/SgCon/SgCon/Repository/CompanyRepository.cs
+++ src/SgCon/SgCon/Repository/CompanyRepository.cs
@@ -1,4 +1,6 @@
-﻿using SgConAPI.EntityFramework;
+﻿using Microsoft.EntityFrameworkCore;
+using SgConAPI.EntityFramework;
+using SgConAPI.EntityFramework.QueryFilter;
 using SgConAPI.Models;
 using SgConAPI.Repository.Base;
 using SgConAPI.Repository.Contracts;
@@ -11,9 +13,44 @@ namespace SgConAPI.Repository
 {
     internal class CompanyRepository : BaseDataService<Company>, ICompanyRepository
     {
-        public CompanyRepository(SgConContext context) : base(context)
+        private readonly IAddressRepository _addressRepository;
+        public CompanyRepository(SgConContext context, IAddressRepository addressRepository) : base(context)
         {
-            
+            _addressRepository = addressRepository;
+        }
+
+        public override Company Get(int id)
+        {
+            return (from c in base.Entities
+                        .Include(c => c.Address)
+                        .Include(c => c.Address.AddressType)
+                    where c.Id == id
+                    select c).FirstOrDefault();
+        }
+
+        public override IQueryable<Company> GetAll(Dictionary<string, object> filters = null)
+        {
+
+            IQueryable<Company> companies = base.Entities.Include(a => a.Address);
+
+            if (filters != null)
+            {
+                var qFilter = new QueryFilterFactory<Company>();
+                foreach (var tuple in filters)
+                {
+                    companies = qFilter.QueryFieldContains(companies, tuple.Key, tuple.Value);
+                }
+            }
+
+            return companies;
+        }
+
+        public override Company Update(Company entity)
+        {
+            var address = _addressRepository.Update(entity.Address);
+            entity.Address = address;
+            return base.Update(entity);
+
         }
     }
 }
--- src/SgCon/SgCon/Repository/EmployeeRepository.cs
+++ src/SgCon/SgCon/Repository/EmployeeRepository.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SgConAPI.EntityFramework;
+using SgConAPI.EntityFramework.QueryFilter;
 using SgConAPI.Models;
 using SgConAPI.Repository.Base;
 using SgConAPI.Repository.Contracts;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace SgConAPI.Repository
 {
     internal class EmployeeRepository : BaseDataService<Employee>, IEmployeeRepository
     {
-        public EmployeeRepository(SgConContext context) : base(context)
-        { }
+        private readonly IAddressRepository _addressRepository;
+        public EmployeeRepository(SgConContext context, IAddressRepository addressRepository) : base(context)
+        {
+            _addressRepository = addressRepository;
+        }
 
         public Employee GetEmployeeByEmailOrUsername(ApplicationUser loginUser)
         {
@@ -29,6 +36,40 @@ namespace SgConAPI.Repository
                                     .ThenInclude(pp => pp.Policy)
                                     .Include(p => p.Profile.Role)
                                     .FirstOrDefault(e => e.UserName == userName);
+        }
+
+        public override Employee Get(int id)
+        {
+            return (from c in base.Entities
+                        .Include(c => c.Address)
+                        .Include(c => c.Address.AddressType)
+                    where c.Id == id
+                    select c).FirstOrDefault();
+        }
+
+        public override IQueryable<Employee> GetAll(Dictionary<string, object> filters = null)
+        {
+
+            IQueryable<Employee> employees = base.Entities.Include(a => a.Address);
+
+            if (filters != null)
+            {
+                var qFilter = new QueryFilterFactory<Employee>();
+                foreach (var tuple in filters)
+                {
+                    employees = qFilter.QueryFieldContains(employees, tuple.Key, tuple.Value);
+                }
+            }
+
+            return employees;
+        }
+
+        public override Employee Update(Employee entity)
+        {
+            var address = _addressRepository.Update(entity.Address);
+            entity.Address = address;
+            return base.Update(entity);
+
         }
     }
 }
